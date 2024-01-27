b4edce83306a9770681cc49fed9f5d48ee5019b8
Marcos Kennedy <marcosnogueira997@gmail.com>
2024-01-27 12:30:08 -0300

    conexão com banco de dados

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import app from './src/app.js';
+import conexao from './infra/conexao.js'
 
 const PORT = 3000;
 
-app.listen(PORT, () =>{
-    console.log(`Servidor iniciado no endereço http://localhost:${PORT}`);
+conexao.connect((error) => {
+    if (error) throw error;
+    app.listen(PORT, () => {
+        console.log('Banco de dados iniciado com sucesso')
+        console.log(`Servidor iniciado no endereço http://localhost:${PORT}`);
+    })
 })
