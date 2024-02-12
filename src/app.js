3552937af359bd160217699d90744de30ac02713
Marcos Kennedy <marcosnogueira997@gmail.com>
2024-02-12 15:31:54 -0300

    modificação na estrutura de pastas
    criação do controller de funcionários

--- src/app.js
+++ src/app.js
@@ -1,62 +1,14 @@
 import express from 'express';
-import conexao from '../infra/conexao.js';
+import FuncionariosController from './app/controllers/funcionariosController.js'; 
 
 const app = express();
 app.use(express.json());
 
-function buscarFuncionarioPorId(id_param){
-    return funcionarios.find(funcionario => funcionario.id == id_param);
-}
-
-function buscarPosicaoDoObjeto(id_param){
-    return funcionarios.findIndex(funcionario => funcionario.id == id_param);
-}
-
 //Rotas
-app.get('/funcionarios', (req, res) => {
-    const sql = 'SELECT * FROM funcionarios';
-    conexao.query(sql, (error, result) => {
-        if (error) throw error;
-        res.status(200).json(result);
-    })
-})
-
-app.get('/funcionarios/:id', (req, res) =>{
-    const id = req.params.id;
-    const sql = 'SELECT * FROM funcionarios WHERE id=?'
-    conexao.query(sql, id, (error, result) =>{
-        if (error) throw error;
-        let linha = result[0];
-        res.status(200).json(linha);
-    })
-})
-
-app.post('/funcionarios',(req, res)=>{
-    const dados = req.body;
-    const sql = 'INSERT INTO funcionarios SET?';
-    conexao.query(sql, dados, (error, result) =>{
-        if(error) throw error;
-        res.status(201).send('Funcionário cadastrado com sucesso');
-    })
-})
-
-app.delete('/funcionarios/:id', (req, res) =>{
-    const id = req.params.id;
-    const sql = 'DELETE FROM funcionarios WHERE id=?';
-    conexao.query(sql, id, (error, result) =>{
-        if(error) throw error;
-        res.status(200).send('Funcionário excluído do banco de dados')
-    })
-})
-
-app.put('/funcionarios/:id', (req, res) =>{
-    const id = req.params.id;
-    const dados = req.body;
-    const sql = 'UPDATE funcionarios SET?  WHERE id=?'
-    conexao.query(sql, [dados, id], (error, result) =>{
-        if(error) throw error;
-        res.status(200).json(result);
-    })
-})
+app.get('/funcionarios', FuncionariosController.index);
+app.get('/funcionarios/:id', FuncionariosController.show);
+app.post('/funcionarios', FuncionariosController.store);
+app.put('/funcionarios/:id', FuncionariosController.update);
+app.delete('/funcionarios/:id', FuncionariosController.delete);
 
 export default app;
---
+++ src/app/controllers/funcionariosController.js
@@ -0,0 +1,48 @@
+import conexao from '../database/conexao.js';
+
+class FuncionáriosController{
+    index(req, res) {
+        const sql = 'SELECT * FROM funcionarios';
+        conexao.query(sql, (error, result) => {
+            if (error) throw error;
+            res.status(200).json(result);
+        })
+    }
+    show(req, res) {
+        const id = req.params.id;
+        const sql = 'SELECT * FROM funcionarios WHERE id=?'
+        conexao.query(sql, id, (error, result) =>{
+            if (error) throw error;
+            let linha = result[0];
+            res.status(200).json(linha);
+        })
+    }
+    store(req, res) {
+        const dados = req.body;
+        const sql = 'INSERT INTO funcionarios SET?';
+        conexao.query(sql, dados, (error, result) =>{
+            if(error) throw error;
+            res.status(201).send('Funcionário cadastrado com sucesso');
+        })
+    }
+    update(req, res) {
+        const id = req.params.id;
+        const dados = req.body;
+        const sql = 'UPDATE funcionarios SET?  WHERE id=?'
+        conexao.query(sql, [dados, id], (error, result) =>{
+            if(error) throw error;
+            res.status(200).json(result);
+        })
+    }
+    delete(req, res) {
+        const id = req.params.id;
+        const sql = 'DELETE FROM funcionarios WHERE id=?';
+        conexao.query(sql, id, (error, result) =>{
+            if(error) throw error;
+            res.status(200).send('Funcionário excluído com sucesso')
+        })
+    }
+}
+
+// padrão Singleton
+export default new FuncionáriosController();
